f2f6b76b8081451f03b77ad5fab14bb391e3f42d
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-08-27 14:13:21 -0600

    remove totp package, this was moved to iam
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- totp/doc.go
+++
@@ -1,2 +0,0 @@
-// Package totp provides code generation for TOTP (RFC 6238) and HOTP (RFC 4226)
-package totp
--- totp/testing/api/totp_api.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "net/http"
-
-func TOTP(w http.ResponseWriter, r *http.Request) {
-
-}
