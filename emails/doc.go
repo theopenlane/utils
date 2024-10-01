2a861d281d27c04b2b55d733179a530d172489bb
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-10-01 09:28:57 -0600

    remove old sendgrid + emails, replaced by newman + emailtemplates repos (#18)
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- emails/doc.go
+++
@@ -1,4 +0,0 @@
-// Package emails contains all of the logic and templating for sending emails
-// It is built to allow for other email providers other than SendGrid if required but is mostly specific to SendGrid
-// as that's our email provider of choice
-package emails
--- sendgrid/errors.go
+++
@@ -1,9 +0,0 @@
-package sendgrid
-
-import (
-	"errors"
-)
-
-var (
-	ErrNoRecipientFound = errors.New("no recipient found")
-)
