83dcf81db44bfd5cdb1ee3c2a4efc502e37254b7
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-03-21 09:42:06 -0600

    some upgrades

--- dumper/doc.go
+++ dumper/doc.go
@@ -1,2 +1,4 @@
 // Package dumper is a utility package for dumping http request contents
 package dumper
+
+// The dumper package is a utility for dumping HTTP request contents, useful for debugging and logging purposes.
