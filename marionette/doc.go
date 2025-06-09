833d7ca3e983af242f0bd9cb101092558719eae1
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-06-09 10:08:27 -0500

    fix bad redis variable and remove marionette package

--- marionette/doc.go
+++
@@ -1,2 +0,0 @@
-// Package marionette is a golang based task manager with scheduling, backoff, future scheduling built in. This is a somewhat temporary solution until an external state management solution can be implemented using Redis or similar
-package marionette
